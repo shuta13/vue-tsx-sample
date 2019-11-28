658effd374493dd8070e9fe7549aa291f755b252
shuta13 <hollystarsun@gmail.com>
2019-11-28 18:14:03 +0900

    feat: クラストグル追加

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -7,13 +7,21 @@ export default tsx.component({
   name: "HelloWorld",
   data() {
     return {
-      msg: "HelloWorld"
+      msg: "HelloWorld",
+      isActive: false
     };
   },
   render(h: CreateElement): VNode {
+    let className = null;
+    if (this.isActive) { className = "msg" }
     return (
-      <div class="msg">
-        {this.msg}
+      <div>
+        <div class={className}>
+          {this.msg}
+        </div>
+        <button onClick={() => this.isActive = !this.isActive}>
+          toggle
+        </button>
       </div>
     );
   }
