4cd0e94069c35c0950b194b3cd785515e8fe3c7a
shuta13 <hollystarsun@gmail.com>
2020-01-01 23:56:07 +0900

    warn: this is not working

--- src/components/CssInJs.tsx
+++ src/components/CssInJs.tsx
@@ -1,39 +1,23 @@
 import * as tsx from "vue-tsx-support";
 import { VNode, CreateElement } from "vue";
 
-import addStyleRules from "./modules/addStyleRules";
+import styled from "styled-components";
 
-const styles = {
-  msg: {
-    color: "rgb(59, 165, 119)",
-    animation: "horizontal 1.2s ease-in-out infinite alternate"
-  }
-};
+const Msg = styled.div`
+  color: green;
+`;
 
 export default tsx.component({
   name: "CssInJs",
   data() {
     return {
-      msg: "HelloWorld(CssInJs)",
-      horizontalFrame: `
-        @keyframes horizontal {
-          0% {
-            transform:translateX(-8px);
-          }
-          100% {
-            transform:translateX(8px);
-          }
-        }
-      `
+      msg: "HelloWorld(CssInJs)"
     };
-  },
-  mounted() {
-    addStyleRules(this.horizontalFrame);
   },
   render(h: CreateElement): VNode {
     return (
       <div>
-        <div style={styles.msg}>{this.msg}</div>
+        <Msg>{this.msg}</Msg>
       </div>
     );
   }
